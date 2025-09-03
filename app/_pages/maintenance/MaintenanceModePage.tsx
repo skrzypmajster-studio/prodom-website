e11ae00b20cbf551431f2965b3b45788b3e78b1a
SkrzypMajster <skrzypmajster@gmail.com>
2025-09-03 15:19:42 +0200

    feat: add page for maintenance mode

---
+++ app/_pages/maintenance/MaintenanceModePage.tsx
@@ -0,0 +1,5 @@
+export const MaintenanceModePage = () => (
+    <main className="flex justify-center items-center h-[100vh]">
+        <img src="/maintenance-mode-background.jpg" alt="Maintenance mode image" /> 
+    </main>
+)
